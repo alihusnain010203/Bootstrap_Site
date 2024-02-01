069611467d2d13897389e121425dabebc95fa501
Ali <ali.husnain.010203@gmail.com>
2024-02-01 12:14:54 +0500

    Makes all pages

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const menu=document.querySelector('.hamburger');
 const navLinks=document.querySelector('.navbar-2-links');
-
 menu.addEventListener('click',()=>{
     navLinks.classList.toggle('navbar-2-links-active');
     navLinks.classList.add('col-12')
     menu.classList.remove('navbar-2-links');
 });
+
 
